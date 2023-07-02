8e6637cd7196a8595f417ef6ac5908c78f68aab4
Daniel <daniel.marquez@globalr>
2023-07-02 02:08:01 -0400

    Feat: Edited Packagejson script

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const prefix = 'api/spendr';
+  const prefix = 'spendr';
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix(prefix);
   app.useGlobalPipes(new ValidationPipe());
